15f45358b1822c1af1d4b025989dde170a48851d
aya-cs22 <aya786930@gmail.com>
2024-08-30 02:54:34 +0300

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,20 +14,3 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check valid password"""
     password_byte = password.encode()
     return bcrypt.checkpw(password_byte, hashed_password)
-
-# #!/usr/bin/env python3
-# """Encrypting passwords"""
-# import bcrypt
-
-
-# def hash_password(password: str) -> bytes:
-#     """returns a salted, hashed password, which is a byte string."""
-#     password_byte = password.encode()
-#     hashed_password = bcrypt.hashpw(password_byte, bcrypt.gensalt())
-#     return hashed_password
-
-
-# def is_valid(hashed_password: bytes, password: str) -> bool:
-#     """Check valid password"""
-#     password_byte = password.encode()
-#     return bcrypt.checkpw(password_byte, hashed_password)
