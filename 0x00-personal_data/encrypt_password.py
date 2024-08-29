1f18dd010bce323c7ff77c29852c661ff9b852e5
aya-cs22 <aya786930@gmail.com>
2024-08-29 11:27:22 +0300

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,7 +4,21 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """returns a salted, hashed password, which is a byte string."""
-    password_byte = password.encode()
-    hashed_password = bcrypt.hashpw(password_byte, bcrypt.gensalt())
-    return hash_password
+    """Hashes the given password using bcrypt.
+    Args:
+        password (str): The password to be hashed.
+    Returns:
+        bytes: The hashed password.
+    """
+    byt_psd = password.encode()
+    salted = bcrypt.hashpw(byt_psd, bcrypt.gensalt())
+    return salted
+# def hash_password(password: str) -> bytes:
+#     """returns a salted, hashed password, which is a byte string."""
+#     password_byte = password.encode()
+#     hashed_password = bcrypt.hashpw(password_byte, bcrypt.gensalt())
+#     return hash_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    pass
