9bb9dce7af8dba6f72f25b52ae87522df90195ae
aya-cs22 <aya786930@gmail.com>
2024-08-29 08:59:02 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 """modul"""
 import re
+from typing import List
+def filter_datum(fields: List[str], redaction: str,
+                message: str, separator: str) -> str:
+        """returns the log message obfuscated"""
+        pattern = r'(?:{separator}={field})[^{separator}]+' \
+            .format(separator=separator, field='|'.join(fields))
+        return re.sub(pattern, redaction, message)
 
 
-def filter_datum(fields, redaction, message, separator):
-    """returns the log message obfuscated"""
-    pattern = r'(?:{separator}={field})[^{separator}]+' \
-        .format(separator=separator, field='|'.join(fields))
-    return re.sub(pattern, redaction, message)
+# def filter_datum(fields, redaction, message, separator):
+#     """returns the log message obfuscated"""
+#     pattern = r'(?:{separator}={field})[^{separator}]+' \
+#         .format(separator=separator, field='|'.join(fields))
+#     return re.sub(pattern, redaction, message)
