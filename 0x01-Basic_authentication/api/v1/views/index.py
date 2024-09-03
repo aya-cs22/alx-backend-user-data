42e9b8fb8f7f8ad0ba4093b1d4c18cd06992bfc1
aya-cs22 <aya786930@gmail.com>
2024-09-03 15:34:51 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized()-> None:
     """An endpoint raises a 401 (Unauthorized) error"""
     abort(401)
