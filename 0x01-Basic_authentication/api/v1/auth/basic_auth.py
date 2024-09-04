1e360069e6a87ae728f928554a5df97958f65636
aya-cs22 <aya786930@gmail.com>
2024-09-04 05:53:09 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -69,3 +69,17 @@ class BasicAuth(Auth):
             if u.is_valid_password(user_pwd):
                 return u
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieve the User instance for the request"""
+        if request is None:
+            return None
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        if base64_auth_header is None:
+            return None
+        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        if decoded_auth_header is None:
+            return None
