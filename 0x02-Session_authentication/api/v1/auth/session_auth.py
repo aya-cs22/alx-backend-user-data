413c4d3f531daf76b4494b2b7405547627557ad8
aya-cs22 <aya786930@gmail.com>
2024-09-05 01:09:26 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
         """that creates a Session ID for a user_id"""
         if user_id is None:
             return None
-        if user_id is not str:
+        if not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
