a838316dc4104be11e9111b88906947c5bdd34be
aya-cs22 <aya786930@gmail.com>
2024-09-04 05:15:01 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -18,33 +18,17 @@ class BasicAuth(Auth):
             return None
         return authorization_header[len("Basic "):]
 
+
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str
-                                           ) -> str:
-        """ Decodes the value of a base64 string """
+                                           base64_authorization_header:
+                                               str) -> str:
+        """Decode the Base64 string into a UTF-8 string"""
         if base64_authorization_header is None:
             return None
         if not isinstance(base64_authorization_header, str):
             return None
-
         try:
-            encoded = base64_authorization_header.encode('utf-8')
-            decoded64 = b64decode(encoded)
-            decoded = decoded64.decode('utf-8')
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
         except BaseException:
             return None
-
-        return decoded
-    # def decode_base64_authorization_header(self,
-    #                                        base64_authorization_header:
-    #                                            str) -> str:
-    #     """Decode the Base64 string into a UTF-8 string"""
-    #     if base64_authorization_header is None:
-    #         return None
-    #     if not isinstance(base64_authorization_header, str):
-    #         return None
-    #     try:
-    #         decoded_bytes = base64.b64decode(base64_authorization_header)
-    #         return decoded_bytes.decode('utf-8')
-    #     except Exception:
-    #         return None
