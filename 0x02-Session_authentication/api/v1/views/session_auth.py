9628d24f9888bad7bf92119992d5b3c6619e2601
aya-cs22 <aya786930@gmail.com>
2024-09-05 03:00:53 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,45 +1,68 @@
-##!/usr/bin/env python3
-"""New view for Session Authentication"""
-from flask import Blueprint, request, jsonify
-from os import getenv
-from api.v1.app import auth
+#!/usr/bin/env python3
+""" Module of Session authentication views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
+from os import getenv
 
-# Define the Blueprint for session authentication
-session_auth = Blueprint('session_auth', __name__)
 
-@session_auth.route('/auth_session/login', methods=['POST'])
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
-    """Handle user login and session creation"""
+    """ POST /auth_session/login
+    Return
+        - Logged in user
+    """
     email = request.form.get('email')
-    password = request.form.get('password')
-    
-    # Validate email
+
     if not email:
         return jsonify({"error": "email missing"}), 400
-    
-    # Validate password
+
+    password = request.form.get('password')
+
     if not password:
         return jsonify({"error": "password missing"}), 400
 
-    # Retrieve User instance based on email
-    user = User.search(email=email)
-    if not user:
+    try:
+        found_users = User.search({'email': email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-    
-    # Ensure password is valid
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
 
-    # Create a Session ID
+    if not found_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in found_users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    user = found_users[0]
     session_id = auth.create_session(user.id)
 
-    # Prepare the response
+    SESSION_NAME = getenv("SESSION_NAME")
+
     response = jsonify(user.to_json())
-    cookie_name = getenv('SESSION_NAME', '_my_session_id')
-    response.set_cookie(cookie_name, session_id)
+    response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    Return:
+        - Empty dictionary if succesful
+    """
+    from api.v1.app import auth
+
+    deleted = auth.destroy_session(request)
+
+    if not deleted:
+        abort(404)
+
+    return jsonify({}), 200
 #!/usr/bin/env python3
 # """New view for Session Authentication"""
 # from flask import Blueprint, request, jsonify, make_response
