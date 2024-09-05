bcc26b8eb92f2bfdb3180d266b9fc9b702690128
aya-cs22 <aya786930@gmail.com>
2024-09-05 03:12:39 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -21,8 +21,11 @@ def login():
         return jsonify({"error": "no user found for this email"}), 404
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+    for u in user:    
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    user = user[0]
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
     cookie_name = getenv('SESSION_NAME', '_my_session_id')
