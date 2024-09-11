a6ce24d19a01172c21a8a122a139e797a91c0483
aya-cs22 <aya786930@gmail.com>
2024-09-11 09:15:19 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,3 +14,20 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with the given email and password."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError()
+        except NoResultFound:
+            hash_password = self._hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hash_password)
+        return user
