6fbc390b895f2445ccffffc69210f7984a1933a6
aya-cs22 <aya786930@gmail.com>
2024-09-11 09:19:02 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -10,10 +10,16 @@ from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
-    """Hash password"""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    """ Returns a salted hash of the input password """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
+
+
+def _generate_uuid() -> str:
+    """Returns a string representation of a new UUID"""
+    UUID = uuid4()
+    return str(UUID)
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -23,12 +29,51 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Register a new user with the given email and password."""
+        """ Registers a user in the database
+        Returns: User Object
+        """
+
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            hash_password = _hash_password(password)
-            user = self._db.add_user(email=email, hashed_password=hash_password)
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
             return user
+
         else:
-            raise ValueError()
+            raise ValueError(f'User {email} already exists')
+        # #!/usr/bin/env python3
+# """ Authentication Module """
+
+# import bcrypt
+# from db import DB
+# from sqlalchemy.orm.exc import NoResultFound
+# from typing import Union
+# from user import User
+# from uuid import uuid4
+
+
+# def _hash_password(password: str) -> str:
+#     """Hash password"""
+#     salt = bcrypt.gensalt()
+#     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+#     return hashed_password
+
+# class Auth:
+#     """Auth class to interact with the authentication database.
+#     """
+
+#     def __init__(self):
+#         self._db = DB()
+
+#     def register_user(self, email: str, password: str) -> User:
+#         """Register a new user with the given email and password."""
+#         try:
+#             user = self._db.find_user_by(email=email)
+#         except NoResultFound:
+#             hash_password = _hash_password(password)
+#             user = self._db.add_user(email=email, hashed_password=hash_password)
+#             return user
+#         else:
+#             raise ValueError()
