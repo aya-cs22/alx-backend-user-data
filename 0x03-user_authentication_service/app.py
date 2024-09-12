1c4d25c7e9e3322d125826ea1d512dc06afbb2f2
aya-cs22 <aya786930@gmail.com>
2024-09-12 23:43:56 +0300

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,17 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """User profile"""
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        return abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        return abort(403)
+    AUTH.destroy_session(user.id)
+    return jsonify({"email": user.email}), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
