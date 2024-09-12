3089d7580c6456f042352c4d5a6451f741c24f2c
aya-cs22 <aya786930@gmail.com>
2024-09-13 00:18:04 +0300

    main

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,10 +1,54 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-from user import User
+import requests
 
-print(User.__tablename__)
+BASE_URL = "http://localhost:5000"  
+def register_user(email: str, password: str) -> None:
+    response = requests.post(f"{BASE_URL}/users", data={"email": email, "password": password})
+    assert response.status_code == 200
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+def log_in_wrong_password(email: str, password: str) -> None:
+    response = requests.post(f"{BASE_URL}/sessions", data={"email": email, "password": password})
+    assert response.status_code == 401
+
+def log_in(email: str, password: str) -> str:
+    response = requests.post(f"{BASE_URL}/sessions", data={"email": email, "password": password})
+    assert response.status_code == 200
+    return response.cookies.get("session_id")
+
+def profile_unlogged() -> None:
+    response = requests.get(f"{BASE_URL}/profile")
+    assert response.status_code == 403
+
+def profile_logged(session_id: str) -> None:
+    cookies = {"session_id": session_id}
+    response = requests.get(f"{BASE_URL}/profile", cookies=cookies)
+    assert response.status_code == 200
+
+def log_out(session_id: str) -> None:
+    cookies = {"session_id": session_id}
+    response = requests.delete(f"{BASE_URL}/sessions", cookies=cookies)
+    assert response.status_code == 200
+
+def reset_password_token(email: str) -> str:
+    response = requests.post(f"{BASE_URL}/reset_password", data={"email": email})
+    assert response.status_code == 200
+    return response.json().get("reset_token")
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    data = {"email": email, "reset_token": reset_token, "new_password": new_password}
+    response = requests.put(f"{BASE_URL}/reset_password", data=data)
+    assert response.status_code == 200
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
