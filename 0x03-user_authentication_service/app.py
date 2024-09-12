27640efff603696333277ed6177b407c45b43dbc
aya-cs22 <aya786930@gmail.com>
2024-09-13 00:32:02 +0300

    main

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,16 +91,19 @@ def get_reset_password_token():
 def update_password() -> str:
     """Update password end-point"""
     try:
-        email = request.form.get('email')
-        reset_token = request.form.get('reset_token')
-        new_password = request.form.get('new_password')
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
     except KeyError:
         abort(400)
+
     try:
-        Auth.update_password(reset_token, new_password)
+        AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"}), 200
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
 
 
 if __name__ == "__main__":
