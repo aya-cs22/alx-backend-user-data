af412f7af54bbc528bc9fb77ee46acc9445db884
aya-cs22 <aya786930@gmail.com>
2024-09-12 16:17:06 +0300

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@ from flask import (Flask,
                    jsonify,
                    request,
                    abort,
-                   redirect)
+                   make_response)
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -36,12 +36,15 @@ def login():
     """Handles login and session creation"""
     email = request.form.get('email')
     password = request.form.get('password')
-    if not auth.valid_login(email, password):
+    
+    if not AUTH.valid_login(email, password):
         abort(401)
-    session_id = auth.create_session(email)
-    respose = make_response(jsonify({"email": email, "message": "logged in"}))
-    respose.set_cookie("session_id", session_id)
-    return respose
+    
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
