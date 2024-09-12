4c95fffcb61e7c1d7eea0600e0bdce737874c176
aya-cs22 <aya786930@gmail.com>
2024-09-12 14:46:25 +0300

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,5 +30,7 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
