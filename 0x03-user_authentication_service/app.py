72838f31b43db9d55b25a4db4cd361cf8fd53cb4
aya-cs22 <aya786930@gmail.com>
2024-09-12 16:14:58 +0300

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -31,6 +31,17 @@ def users():
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Handles login and session creation"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not auth.valid_login(email, password):
+        abort(401)
+    session_id = auth.create_session(email)
+    respose = make_response(jsonify({"email": email, "message": "logged in"}))
+    respose.set_cookie("session_id", session_id)
+    return respose
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
