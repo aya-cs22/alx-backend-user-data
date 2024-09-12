ab35bcadb81dce4349266049ac4a734ac2c41263
aya-cs22 <aya786930@gmail.com>
2024-09-12 14:48:39 +0300

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,16 +20,27 @@ def hello_world() -> str:
 
 @app.route('/users', methods=['POST'])
 def users():
-    """POST /users endpoint to register a user"""
+    '''register'''
     email = request.form.get('email')
     password = request.form.get('password')
-    if not email or not password:
-        return jsonify({"message": "email and password are required"}), 400
+
     try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"}), 201
-    except ValueError:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
         return jsonify({"message": "email already registered"}), 400
+# @app.route('/users', methods=['POST'])
+# def users():
+#     """POST /users endpoint to register a user"""
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+#     if not email or not password:
+#         return jsonify({"message": "email and password are required"}), 400
+#     try:
+#         user = AUTH.register_user(email, password)
+#         return jsonify({"email": user.email, "message": "user created"}), 201
+#     except ValueError:
+#         return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
