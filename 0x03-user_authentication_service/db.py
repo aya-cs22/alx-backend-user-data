800abcc7ae6d009ed9f3ae7d1544b956ec0eb62e
aya-cs22 <aya786930@gmail.com>
2024-09-12 14:34:31 +0300

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,4 +1,4 @@
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 """ Database for ORM """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -19,89 +19,33 @@ class DB:
         Base.metadata.create_all(self._engine)
         self.__session = None
 
+    
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ Session Getter Method """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        '''Creates and adds a user to the database'''
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
-
     def find_user_by(self, **kwargs) -> User:
-        '''searches for a user'''
+        """To find a user in the Users table"""
         try:
-            return self._session.query(User).filter_by(**kwargs).one()
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
         except NoResultFound:
             raise NoResultFound
         except InvalidRequestError:
             raise InvalidRequestError
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        '''updates a user'''
-        user = self.find_user_by(id=user_id)
-        for k in kwargs:
-            if not hasattr(user, k):
-                raise ValueError
-        for k, v in kwargs.items():
-            user.k = v
-        self._session.commit()
-        return None
-    # #!/usr/bin/env python3
-# """ Database for ORM """
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.exc import InvalidRequestError
-# from sqlalchemy.orm.exc import NoResultFound
-# from typing import TypeVar
-# from user import Base, User
-
-
-# class DB:
-#     """ DB Class for Object Reational Mapping """
-
-#     def __init__(self):
-#         """ Constructor Method """
-#         self._engine = create_engine("sqlite:///a.db", echo=False)
-#         Base.metadata.drop_all(self._engine)
-#         Base.metadata.create_all(self._engine)
-#         self.__session = None
-
-    
-#     @property
-#     def _session(self):
-#         """ Session Getter Method """
-#         if self.__session is None:
-#             DBSession = sessionmaker(bind=self._engine)
-#             self.__session = DBSession()
-#         return self.__session
-
-#     def find_user_by(self, **kwargs) -> User:
-#         """To find a user in the Users table"""
-#         try:
-#             user = self._session.query(User).filter_by(**kwargs).one()
-#             return user
-#         except NoResultFound:
-#             raise NoResultFound
-#         except InvalidRequestError:
-#             raise InvalidRequestError
-
-#     def update_user(self, user_id: int, **kwargs) -> None:
-#         """Update user information in the database"""
-#         try:
-#             user = self.find_user_by(id=user_id)
-#             for key, value in kwargs.items():
-#                 if not hasattr(user, key):
-#                     raise ValueError
-#                 setattr(user, key, value)
-#             self.__session.commit()
-#         except NoResultFound:
-#             raise NoResultFound
+        """Update user information in the database"""
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError
+                setattr(user, key, value)
+            self.__session.commit()
+        except NoResultFound:
+            raise NoResultFound
